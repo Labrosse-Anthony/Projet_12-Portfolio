237d2ee2b4df2137c1566a29ecae8404c147e7dd
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-06-09 17:25:09 +0200

    Ajout du composent Projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Skills from './components/Skills/Skills.jsx';
+import Projects from './components/Projects/Projects.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import "./styles/variables.css";
 import "./styles/index.css";
@@ -13,6 +14,7 @@ function App() {
       <main>
         <Hero />
         <Skills />
+        <Projects />
       </main>
       <Footer />
     </div>
