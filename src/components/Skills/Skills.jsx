eeddfd2e2070a052e2c94c6ad13b7002c6191260
Anthony labrosse <labrosse.anthony.dev@gmail.com>
2026-06-22 09:23:40 +0200

    Amélioration de l'accessibilité : ajout repère main et attributs alt vides pour les icônes

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -7,31 +7,31 @@ const Skills = () => {
       <h2 className="skills__title">COMPÉTENCES</h2>
       <div className="skills__container">
         <div className="skill-badge">
-            <img src="./img/logo-html.webp" alt="HTML" className="skill-icon" />
+            <img src="./img/logo-html.webp" alt="" className="skill-icon" />
             <span className="skill-name">HTML</span>
         </div>
         <div className="skill-badge">
-            <img src="./img/logo-css.webp" alt="CSS" className="skill-icon" />
+            <img src="./img/logo-css.webp" alt="" className="skill-icon" />
             <span className="skill-name">CSS</span>
         </div>
         <div className="skill-badge">
-          <img src="./img/logo-scss.webp" alt="SCSS" className="skill-icon" />
+          <img src="./img/logo-scss.webp" alt="" className="skill-icon" />
           <span className="skill-name">SCSS</span>
         </div>
         <div className="skill-badge">
-            <img src="./img/logo-java-script.webp" alt="JavaScript" className="skill-icon" />
+            <img src="./img/logo-java-script.webp" alt="" className="skill-icon" />
             <span className="skill-name">JAVASCRIPT</span>
         </div>
         <div className="skill-badge">
-            <img src="./img/logo-figma.webp" alt="Figma" className="skill-icon" />
+            <img src="./img/logo-figma.webp" alt="" className="skill-icon" />
           <span className="skill-name">FIGMA</span>
         </div>
         <div className="skill-badge">
-            <img src="./img/logo-react.webp" alt="React" className="skill-icon" />
+            <img src="./img/logo-react.webp" alt="" className="skill-icon" />
             <span className="skill-name">REACT</span>
         </div>
         <div className="skill-badge">
-            <img src="./img/logo-github.webp" alt="GitHub" className="skill-icon" />
+            <img src="./img/logo-github.webp" alt="" className="skill-icon" />
             <span className="skill-name">GITHUB</span>
         </div>
       </div>
